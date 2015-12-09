d2efcde0e61beeff6a674d4073d1d8544b6884ce
Liavona ZHELTANOSAVA <zheltanosava.liavona@etu.unice.fr>
2015-12-10 00:41:00 +0100

    user peur saisir ses parametres

--- src/oop/flu/Field.java
+++ src/oop/flu/Field.java
@@ -21,7 +21,7 @@ public class Field {
 * @param height height of the field
 * @param width width of the field
 */
-    public Field(int height, int width) {
+    public Field(int width, int height) {
     	this.height = height;
     	this.width = width;
     	field = new LivingBeing[width][height];
--- src/oop/flu/TestNewClasses.java
+++ src/oop/flu/TestNewClasses.java
@@ -1,14 +1,39 @@
 package oop.flu;
 
+import java.util.*;
+
 public class TestNewClasses {
 
     public static void main(String[] args) throws InterruptedException {
-    	// create new simulation with field 5x5 and poputation rate = 0.5
-        Simulation simul = new Simulation(6,5, 0.5);
+        Scanner scan = new Scanner(System.in);
+        String param = "";  
+        System.out.print("Do you want default simulation? o/n: ");  
+        param = scan.nextLine();
+        Simulation simul;
+        // create a simulation with default parameters
+        if (param.equals("o") || param.equals("O")) {
+            simul = new Simulation();
+        }
+        // user enter his parameters
+    	else if (param.equals("n") || param.equals("N")) {
+            scan.useLocale(Locale.US);
+            int width = 0;
+            int height = 0;
+            double poputationRate = 0;
+            System.out.print("Enter width (int): ");
+            width = scan.nextInt();
+            System.out.print("Enter height (int): ");
+            height = scan.nextInt(); 
+            System.out.print("Enter population rate (0 < n < 1): ");
+            poputationRate = scan.nextDouble(); 
+            simul = new Simulation(width, height, poputationRate);
+        }
+        else {
+            System.out.println("Yolo, retry plz");
+            return;
+        }
         // fill the field of simul
         simul.fillField(simul.getPopulationRate());
-        System.out.println("Width = " + simul.getField().getWidth());
-        System.out.println("Height = " + simul.getField().getHeight());
         // show the field
         System.out.println(simul.getField().toString());
         
@@ -21,6 +46,6 @@ public class TestNewClasses {
         System.out.println("Are they all dead? : " + simul.getField().areAllDead());
         System.out.println("Are they all healthy? : " + simul.getField().areAllHealthy());
         
-        simul.run();
+        // simul.run();
     }
 }
